e6a7fffbe8e6eac8a0ed039c42589c6787be77ac
Ramya-sjr <ramyasjr@gmail.com>
2021-06-26 11:04:24 +0530

    to-do app with context-reducer implemented

---
+++ 6-context-reducer-todo/src/components/TodoForm.js
@@ -0,0 +1,24 @@
+import React, {useState, useContext} from 'react'
+import {FormGroup, Input, Button, Form, InputGroup, InputGroupAddon}   from 'reactstrap'
+import {v4} from 'uuid'
+import {TodoContext} from '../context/TodoContext'
+import {ADD_TODO} from '../context/action.types'
+
+const TodoForm = () => {
+
+    return(
+        <Form>
+            <FormGroup>
+                <InputGroup>
+                    <Input type="text" name="todo" id="todo" placeholder="Your Next Todo" />
+                    <InputGroupAddon addonType="prepend">
+                        <Button color="warning">Add</Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+
+}
+
+export default TodoForm
