c2089a956648efcaab93aca081f83afc10fc42e6
Ramya-sjr <ramyasjr@gmail.com>
2021-10-20 18:30:25 +0530

    new project added for tracking expense

---
+++ expense_tracker/src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+
+
+  const onChangeYearHandler = (event) => {
+    console.log(event.target.value);
+    props.selectedYear(event.target.value);
+  }
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={onChangeYearHandler} value={props.defaultYear}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
